8a9ab259225a83ba3ac24db6529731c320be1233
Hariharan Velu <veluhariharan@gmail.com>
2016-01-13 15:33:17 -0600

    Find Digits Implementation

---
+++ HackerRank_Implementation/HackerRank_Implementation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            long n = Convert.ToInt64(Console.ReadLine());
+
+            CalculateDivisors(n);
+        }
+
+
+    }
+
+    static void CalculateDivisors(long n)
+    {
+        int evenNumOfDivisors = 0;
+
+        string number = n.ToString();
+
+        for (int i = 0; i < number.Length; i++)
+        {
+            if (Convert.ToInt32(number[i].ToString()) != 0 && (n % Convert.ToInt32(number[i].ToString()) == 0))
+            {
+                evenNumOfDivisors++;
+            }
+        }
+        Console.WriteLine(evenNumOfDivisors);
+    }
+}
